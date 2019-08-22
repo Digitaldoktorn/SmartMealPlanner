75e4efa4508b4fc8274146a10f2839a4c2e889c3
Anders Eriksson <digitaldoktorn@gmail.com>
2019-08-22 20:51:38 +0200

    simplified app, added click events

--- main.js
+++ main.js
@@ -1,53 +1,110 @@
-/*
-const button = document.getElementsByTagName('button')
-Array.from(button).forEach(function(item){
-    console.log(item.textContent)
-})
-const headline = document.getElementsByTagName('h1')
-Array.from(headline).forEach(function(item){
-    console.log(item)
-})
-*/
-
 const mytable = document.getElementsByTagName('table')[0]
-// console.log(mytable);
-
 const mytablebody = mytable.getElementsByTagName('tbody')[0]
-// console.log(mytablebody);
 
-const myrow = mytablebody.getElementsByTagName('tr')[0]
-// console.log(myrow);
+const rowOne = mytablebody.getElementsByTagName('tr')[0]
+const rowTwo = mytablebody.getElementsByTagName('tr')[1]
+const rowThree = mytablebody.getElementsByTagName('tr')[2]
+const rowFour = mytablebody.getElementsByTagName('tr')[3]
+const rowFive = mytablebody.getElementsByTagName('tr')[4]
+const rowSix = mytablebody.getElementsByTagName('tr')[5]
+const rowSeven = mytablebody.getElementsByTagName('tr')[6]
 
-const mycel = myrow.getElementsByTagName('td')[0]
-// console.log(mycel);
-
-// first item element of the childNodes list of mycel
-myceltext = mycel.childNodes[0]
-// console.log(myceltext);
-
-mycel.innerHTML += '_test'
+const foodMonday = rowOne.getElementsByTagName('td')[0]
+const foodTuesday = rowTwo.getElementsByTagName('td')[0]
+const foodWednesday = rowThree.getElementsByTagName('td')[0]
+const foodThursday = rowFour.getElementsByTagName('td')[0]
+const foodFriday = rowFive.getElementsByTagName('td')[0]
+const foodSaturday = rowSix.getElementsByTagName('td')[0]
+const foodSunday = rowSeven.getElementsByTagName('td')[0]
 
 // checking what node type, what node name and if it has child nodes
 // https://www.w3schools.com/XML/dom_nodetype.asp
-console.log('Node typ: ', mytable.nodeType, 'Node name: ', mytable.nodeName, 'Has child nodes: ', mytable.hasChildNodes())
-console.log('Node typ: ', mytablebody.nodeType, 'Node name: ', mytablebody.nodeName, 'Has child nodes: ', mytablebody.hasChildNodes())
-console.log('Node typ: ', myrow.nodeType, 'Node name: ', myrow.nodeName, 'Has child nodes: ', myrow.hasChildNodes())
-console.log('Node typ: ', mycel.nodeType, 'Node name: ', mycel.nodeName, 'Has child nodes: ', mycel.hasChildNodes())
-console.log('Node typ: ', myceltext.nodeType, 'Node name: ', myceltext.nodeName, 'Has child nodes: ', myceltext.hasChildNodes())
+console.log('Node type: ', mytable.nodeType, 'Node name: ', mytable.nodeName, 'Has child nodes: ', mytable.hasChildNodes())
+console.log('Node type: ', mytablebody.nodeType, 'Node name: ', mytablebody.nodeName, 'Has child nodes: ', mytablebody.hasChildNodes())
+console.log('Node type: ', rowOne.nodeType, 'Node name: ', rowOne.nodeName, 'Has child nodes: ', rowOne.hasChildNodes())
+console.log('Node type: ', foodMonday.nodeType, 'Node name: ', foodMonday.nodeName, 'Has child nodes: ', foodMonday.hasChildNodes())
 
 // checking the parent nodes and parent elements and children nodes and children elements
 console.log('Mytable parent element is: ', mytable.parentElement)
 console.log('Mytablebody parent node is: ', mytablebody.parentNode)
 console.log('Mytablebody parents parent node is: ',mytablebody.parentElement.parentElement)
 console.log('Mytablebody child nodes are: ', mytablebody.childNodes)
-console.log('Mytablebody child elements are: ', mytablebody.children)
+console.log('Mytablebody child elements are: ', mytablebody.children[0].innerText)
 
 // checking siblings
 console.log('Mytablebody previous sibling is: ', mytablebody.previousSibling)
 console.log('Mytablebody previous sibling element is: ', mytablebody.previousElementSibling)
-console.log('Myrow next sibling element: ', myrow.nextElementSibling)
-console.log('Mycel next sibling element: ', mycel.nextElementSibling)
-console.log('Mycel previous sibling element: ', mycel.previousElementSibling)
+console.log('rowOne next sibling element: ', rowOne.nextElementSibling)
+console.log('foodMonday next sibling element: ', foodMonday.nextElementSibling)
+console.log('foodMonday previous sibling element: ', foodMonday.previousElementSibling)
+
+// selected week day
+const selectMonday = document.getElementById('gridRadios_2_1')
+const selectTuesday = document.getElementById('gridRadios_2_2')
+const selectWednesday = document.getElementById('gridRadios_2_3')
+const selectThursday = document.getElementById('gridRadios_2_4')
+const selectFriday = document.getElementById('gridRadios_2_5')
+const selectSaturday = document.getElementById('gridRadios_2_6')
+const selectSunday = document.getElementById('gridRadios_2_7')
+
+
+// fetching user input
+const addMeal = document.forms['myform']
+addMeal.addEventListener('submit', function(e){
+    const value = addMeal.querySelector('#input-food').value;
+
+    if(selectMonday.checked){
+        foodMonday.innerHTML = value;
+    }
+    if(selectTuesday.checked){
+        foodTuesday.innerHTML = value;
+    }
+    if(selectWednesday.checked){
+        foodWednesday.innerHTML = value;
+    }
+    if(selectThursday.checked){
+        foodThursday.innerHTML = value;
+    }
+    if(selectFriday.checked){
+        foodFriday.innerHTML = value;
+    }
+    if(selectSaturday.checked){
+        foodSaturday.innerHTML = value;
+    }
+    if(selectSunday.checked){
+        foodSunday.innerHTML = value;
+    }
+    
+})
+
+const addContent = document.forms['myform']
+addContent.addEventListener('submit', function(e){
+    e.preventDefault()
+    const value = addContent.querySelector('#input-content').value;
+
+    if(selectMonday.checked){
+        foodMonday.nextElementSibling.innerHTML = value
+    }
+    if(selectTuesday.checked){
+        foodTuesday.nextElementSibling.innerHTML = value
+    }
+    if(selectWednesday.checked){
+        foodWednesday.nextElementSibling.innerHTML = value
+    }
+    if(selectThursday.checked){
+        foodThursday.nextElementSibling.innerHTML = value
+    }
+    if(selectFriday.checked){
+        foodFriday.nextElementSibling.innerHTML = value
+    }
+    if(selectSaturday.checked){
+        foodSaturday.nextElementSibling.innerHTML = value
+    }
+    if(selectSunday.checked){
+        foodSunday.nextElementSibling.innerHTML = value
+    }
+    
+})
 
 
 
