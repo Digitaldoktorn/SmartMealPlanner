08d62ace49e20bef76264761f7b01d3c72c9a23c
Anders Eriksson <digitaldoktorn@gmail.com>
2019-08-22 21:55:41 +0200

    removed comments

--- main.js
+++ main.js
@@ -17,27 +17,6 @@ const foodFriday = rowFive.getElementsByTagName('td')[0]
 const foodSaturday = rowSix.getElementsByTagName('td')[0]
 const foodSunday = rowSeven.getElementsByTagName('td')[0]
 
-// checking what node type, what node name and if it has child nodes
-// https://www.w3schools.com/XML/dom_nodetype.asp
-console.log('Node type: ', mytable.nodeType, 'Node name: ', mytable.nodeName, 'Has child nodes: ', mytable.hasChildNodes())
-console.log('Node type: ', mytablebody.nodeType, 'Node name: ', mytablebody.nodeName, 'Has child nodes: ', mytablebody.hasChildNodes())
-console.log('Node type: ', rowOne.nodeType, 'Node name: ', rowOne.nodeName, 'Has child nodes: ', rowOne.hasChildNodes())
-console.log('Node type: ', foodMonday.nodeType, 'Node name: ', foodMonday.nodeName, 'Has child nodes: ', foodMonday.hasChildNodes())
-
-// checking the parent nodes and parent elements and children nodes and children elements
-console.log('Mytable parent element is: ', mytable.parentElement)
-console.log('Mytablebody parent node is: ', mytablebody.parentNode)
-console.log('Mytablebody parents parent node is: ',mytablebody.parentElement.parentElement)
-console.log('Mytablebody child nodes are: ', mytablebody.childNodes)
-console.log('Mytablebody child elements are: ', mytablebody.children[0].innerText)
-
-// checking siblings
-console.log('Mytablebody previous sibling is: ', mytablebody.previousSibling)
-console.log('Mytablebody previous sibling element is: ', mytablebody.previousElementSibling)
-console.log('rowOne next sibling element: ', rowOne.nextElementSibling)
-console.log('foodMonday next sibling element: ', foodMonday.nextElementSibling)
-console.log('foodMonday previous sibling element: ', foodMonday.previousElementSibling)
-
 // selected week day
 const selectMonday = document.getElementById('gridRadios_2_1')
 const selectTuesday = document.getElementById('gridRadios_2_2')
@@ -105,6 +84,3 @@ addContent.addEventListener('submit', function(e){
     }
     
 })
-
-
-
